b3b0eb044e23b49e83e8e743c34e9a2f6487cf0c
neil.mehta <neil.mehta@faithgroupllc.com>
2024-04-25 16:27:37 -0500

    typescript test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-
-
 // Get references to the button and output elements
 const button = document.getElementById('myButton');
 const output = document.getElementById('output');
 
+if(button){ 
 // Add event listener to the button
 button.addEventListener('click', () => {
   // Change the text content of the output div
-  output.textContent = 'You clicked the button!';
-  console.log('is this working...')
+  if(output){
+    output.textContent = 'You clicked the button!';
+    console.log('is this working...')
+  }
 });
+}
